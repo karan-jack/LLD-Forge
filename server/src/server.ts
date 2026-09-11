b8160fc893e947042eb8428a624e01c469b401f0
karanjack08 <karanjackrnc@gmail.com>
2026-09-11 22:53:57 +0530

    dusted again

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,2 +1,8 @@
 import 'dotenv/config';
-import { app } from './app'; const PORT = process.env.PORT || 3000; app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+import { app } from './app';
+
+const PORT = Number(process.env.PORT) || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+});
